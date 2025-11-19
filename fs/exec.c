84596b3fc49c7f37747c1f3df952dce27f3e929f
sarreos <mlisowski331@gmail.com>
2025-11-19 11:31:40 +0100

    Reapply "Add ksu handling in execveat functions"
    This reverts commit 06de70d9fd84e9e0faafa6da439cf388dbd6a36b.

--- fs/exec.c
+++ fs/exec.c
@@ -1958,6 +1958,9 @@ int do_execve(struct filename *filename,
 {
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
+#ifdef CONFIG_KSU
+	ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
+#endif
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
 
@@ -1985,6 +1988,9 @@ static int compat_do_execve(struct filename *filename,
 		.is_compat = true,
 		.ptr.compat = __envp,
 	};
+#ifdef CONFIG_KSU // 32-bit ksud and 32-on-64 support
+	ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
+#endif
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
 
